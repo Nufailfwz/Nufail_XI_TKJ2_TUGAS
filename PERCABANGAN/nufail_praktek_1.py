ffdaad779f527220978d016f952032a0bc59b691
Nufailfwz <148348310+Nufailfwz@users.noreply.github.com>
2023-11-28 00:21:07 +0700

    Add files via upload

---
+++ PERCABANGAN/nufail_praktek_1.py
@@ -0,0 +1,17 @@
+# NAMA : MUHAMADA NUFAIL FAWWAZ  
+# KELAS : XI TKJ 2 
+# NO : 21
+
+# soal : Di toko online, hitung diskon berdasarkan total belanjaan pelanggan 
+
+total_belanjaan = float(input("Total belanjaan anda: "))
+
+if total_belanjaan > 500000:
+    diskon  = total_belanjaan * 0.10
+elif total_belanjaan >= 300000:
+    diskon = total_belanjaan * 0.05
+else:
+    diskon = 0
+
+total_pembayaran = total_belanjaan - diskon
+print(f"Total pembayaran setelah diskon : {total_pembayaran}")
---
+++ PERCABANGAN/nufail_praktek_9.py
@@ -0,0 +1,18 @@
+# NAMA : MUHAMADA NUFAIL FAWWAZ  
+# KELAS : XI TKJ 2 
+# NO : 21
+
+#Buat program Python yang mengambil status persiapan proyek dan menentukan apakah proyek 
+#tersebut dapat diluncurkan. jika status  persiapan "Siap", program harus mencetak "Proyek 
+#diluncurkan". Jika status  persiapan "Tunda", program harus mencetak "Proyek ditunda"
+
+status_persiapan_proyek = str(input("Masukkan status persiapan proyek (Siap/Tunda): ").lower())
+
+if status_persiapan_proyek == "siap":
+    tindakan = "diluncurkan."
+elif status_persiapan_proyek == "tunda":
+    tindakan = "ditunda."
+else:
+    tindakan = "tidak valid."
+
+print(f"Proyek akan {tindakan}")
